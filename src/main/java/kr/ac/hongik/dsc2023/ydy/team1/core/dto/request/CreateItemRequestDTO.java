a5fd93aeb628fc79592c456674a77609282450bb
robinjoon <robin980108@naver.com>
2023-04-18 22:20:54 +0900

    Docs (DTO) : DTO 클래스 JavaDoc 추가

--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/CreateItemRequestDTO.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/CreateItemRequestDTO.java
@@ -3,7 +3,7 @@ package kr.ac.hongik.dsc2023.ydy.team1.core.dto.request;
 import lombok.Getter;
 
 /**
- *
+ * 상품 생성 요청을 표현하는 클래스
  */
 
 @Getter
--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/CreateMultipleItemsRequestDTO.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/CreateMultipleItemsRequestDTO.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import java.util.List;
 
 /**
- *
+ * 여러개의 상품을 한번에 생성하는 요청을 표현하는 클래스
  */
 @Getter
 public class CreateMultipleItemsRequestDTO {
