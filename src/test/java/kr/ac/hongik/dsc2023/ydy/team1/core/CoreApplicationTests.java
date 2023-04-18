57acb24b4ab3679c0b01cfd3529dd874767a2bc5
robinjoon <robin980108@naver.com>
2023-04-18 18:45:06 +0900

    Initial commit

---
+++ src/test/java/kr/ac/hongik/dsc2023/ydy/team1/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hongik.dsc2023.ydy.team1.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
