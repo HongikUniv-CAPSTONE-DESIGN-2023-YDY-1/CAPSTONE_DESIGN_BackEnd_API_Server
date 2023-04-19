1a05b37e738b31edc402e99660b97026622abfc3
robinjoon <robin980108@naver.com>
2023-04-19 18:10:37 +0900

    Fix (DTO) : DTO 클래스 추상화

--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/Promotion.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.hongik.dsc2023.ydy.team1.core.dto.request;
-/**
- * 할인 행사 종류를 표현하는 클래스
- */
-public enum Promotion {
-    ONE_PLUS_ONE,TWO_PLUS_ONE
-}
--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/response/SearchItemResponseDTO.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/response/SearchItemResponseDTO.java
@@ -1,26 +1,13 @@
 package kr.ac.hongik.dsc2023.ydy.team1.core.dto.response;
 
-import kr.ac.hongik.dsc2023.ydy.team1.core.dto.request.Brand;
 import lombok.Getter;
 
 import java.util.List;
-import java.util.Set;
+
 @Getter
 /**
- * 상품 검색 결과를 표현하는 클래스
+ * Item 검색 결과를 표현하는 추상 클래스
  */
-public class SearchItemResponseDTO {
-    private List<SearchItem> searchItems;
-    private static class SearchItem{
-        String name;
-        Set<Brand> promotionBrands;
-        List<StoreResponse> nearStores;
-        int price;
-        int pricePerUnit;
-        private static class StoreResponse{
-            String name;
-            double latitude;
-            double longitude;
-        }
-    }
+public abstract class SearchItemResponseDTO {
+    protected List<SearchItem> searchItems;
 }
