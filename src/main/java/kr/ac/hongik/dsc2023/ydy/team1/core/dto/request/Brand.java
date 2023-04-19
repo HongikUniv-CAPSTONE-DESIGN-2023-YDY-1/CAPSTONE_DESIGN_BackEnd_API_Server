261327849626646d26c8f7856944ba8769c57dfc
robinjoon <robin980108@naver.com>
2023-04-19 18:10:37 +0900

    Fix (DTO) : DTO 클래스 추상화

--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/Brand.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.hongik.dsc2023.ydy.team1.core.dto.request;
-
-/**
- * 상점 브랜드를 의미하는 클래스
- */
-public enum Brand {
-    GS25,CU,SEVEN_ELEVEN,EMART24
-}
--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/CreateItemRequestDTO.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/CreateItemRequestDTO.java
@@ -1,16 +1,8 @@
 package kr.ac.hongik.dsc2023.ydy.team1.core.dto.request;
 
-import lombok.Getter;
-
 /**
- * 상품 생성 요청을 표현하는 클래스
+ * Item 생성 요청을 표현하는 클래스
  */
-
-@Getter
-class CreateItemRequestDTO {
-    private String name;
-    private String image;
-    private int price;
-    private Brand brand;
-    private Promotion promotion;
+public abstract class CreateItemRequestDTO {
+    protected String name;
 }
