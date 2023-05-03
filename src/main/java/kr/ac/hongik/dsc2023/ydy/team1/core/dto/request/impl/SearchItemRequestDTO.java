ea0da4192ae8418c953e80670349210e51214110
robinjoon <robin980108@naver.com>
2023-05-03 22:58:41 +0900

    Fix (SearchItemRequestDTO) : Builder, toString 추가

--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/impl/SearchItemRequestDTO.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/impl/SearchItemRequestDTO.java
@@ -1,8 +1,13 @@
 package kr.ac.hongik.dsc2023.ydy.team1.core.dto.request.impl;
 
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
 /**
  * 클라이언트의 Item 검색 요청을 표현하는 추상 클래스
  */
+@SuperBuilder
+@ToString
 public abstract class SearchItemRequestDTO {
     protected String name;
     protected SearchStrength strength;
