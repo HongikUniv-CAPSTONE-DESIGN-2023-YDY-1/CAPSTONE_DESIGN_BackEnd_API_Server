b14be9335c1c5a5233ac144c96aa5e6757b1ddf6
robinjoon <robin980108@naver.com>
2023-05-03 23:17:06 +0900

    Fix (ItemsCreateRequestDTO) : 일반 클래스로 변경

--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/impl/ItemsCreateRequestDTO.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/request/impl/ItemsCreateRequestDTO.java
@@ -1,13 +1,17 @@
 package kr.ac.hongik.dsc2023.ydy.team1.core.dto.request.impl;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 /**
- * 여러개의 Item을 한번에 생성하는 요청을 표현하는 추상 클래스
+ * 여러개의 Item을 한번에 생성하는 요청을 표현하는 클래스
  */
 @Getter
-public abstract class ItemsCreateRequestDTO {
-    private List<ItemCreateRequestDTO> itemList;
+@Builder
+@ToString
+public class ItemsCreateRequestDTO<T extends ItemCreateRequestDTO> {
+    private final List<T> itemList;
 }
--- src/test/java/kr/ac/hongik/dsc2023/ydy/team1/core/CoreApplicationTests.java
+++ src/test/java/kr/ac/hongik/dsc2023/ydy/team1/core/CoreApplicationTests.java
@@ -1,9 +1,13 @@
 package kr.ac.hongik.dsc2023.ydy.team1.core;
 
+import kr.ac.hongik.dsc2023.ydy.team1.core.dto.request.impl.ItemsCreateRequestDTO;
 import kr.ac.hongik.dsc2023.ydy.team1.core.dto.request.impl.SearchStrength;
 import kr.ac.hongik.dsc2023.ydy.team1.core.dto.request.konbini.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class CoreApplicationTests {
@@ -25,5 +29,21 @@ class CoreApplicationTests {
 				.picture(null)
 				.build();
 		System.out.println(dto);
+	}
+	@Test
+	void createItemsDTOTest(){
+		KonbiniItemCreateRequestDTO dto = KonbiniItemCreateRequestDTO.builder()
+				.name("꼬갈콘")
+				.brand(KonbiniBrand.CU)
+				.promotion(KonbiniPromotion.ONE_PLUS_ONE)
+				.pricePerUnit(1)
+				.picture(null)
+				.build();
+		List<KonbiniItemCreateRequestDTO> list = new ArrayList<>();
+		list.add(dto);
+		ItemsCreateRequestDTO<KonbiniItemCreateRequestDTO> requestDTO = ItemsCreateRequestDTO.<KonbiniItemCreateRequestDTO>builder()
+				.itemList(list)
+				.build();
+		System.out.println(requestDTO);
 	}
 }
