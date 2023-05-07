c257be47105529912be6d0b77dbbb0b0dc3d3a52
robinjoon <robin980108@naver.com>
2023-05-07 17:02:52 +0900

    Feat (Response) : 편의점 상품 검색 DTO 추가

---
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/dto/response/konbini/KonbiniSearchItemResponseDTO.java
@@ -0,0 +1,10 @@
+package kr.ac.hongik.dsc2023.ydy.team1.core.dto.response.konbini;
+
+import kr.ac.hongik.dsc2023.ydy.team1.core.dto.response.port.SearchItemResponseDTO;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+public class KonbiniSearchItemResponseDTO extends SearchItemResponseDTO {
+}
