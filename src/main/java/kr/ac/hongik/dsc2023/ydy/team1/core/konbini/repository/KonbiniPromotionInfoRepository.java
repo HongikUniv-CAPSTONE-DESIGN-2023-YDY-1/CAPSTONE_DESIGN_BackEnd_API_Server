333526764470be3cc50ec05bbea46c1d2cb4bb4f
robinjoon <robin980108@naver.com>
2023-05-18 17:20:03 +0900

    Fix(KonbiniPromotionInfoRepository) : 검색 메서드 추가

--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/konbini/repository/KonbiniPromotionInfoRepository.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/konbini/repository/KonbiniPromotionInfoRepository.java
@@ -2,6 +2,10 @@ package kr.ac.hongik.dsc2023.ydy.team1.core.konbini.repository;
 
 import kr.ac.hongik.dsc2023.ydy.team1.core.konbini.entity.PromotionInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface KonbiniPromotionInfoRepository extends JpaRepository<PromotionInfo,Long> {
+    List<PromotionInfo> findAllByItem_NameContains(String name);
 }
