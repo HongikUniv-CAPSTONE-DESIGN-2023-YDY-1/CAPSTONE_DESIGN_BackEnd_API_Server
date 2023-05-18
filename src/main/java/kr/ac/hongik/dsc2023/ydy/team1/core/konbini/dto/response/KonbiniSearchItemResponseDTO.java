c2cb9752e4fa5df4e33018700126034b52197c4a
robinjoon <robin980108@naver.com>
2023-05-18 17:15:51 +0900

    Fix(SearchItemResponse) : 제네릭으로 수정

--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/konbini/dto/response/KonbiniSearchItemResponseDTO.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/konbini/dto/response/KonbiniSearchItemResponseDTO.java
@@ -1,10 +1,11 @@
 package kr.ac.hongik.dsc2023.ydy.team1.core.konbini.dto.response;
 
+import kr.ac.hongik.dsc2023.ydy.team1.core.architecture.dto.response.SearchItem;
 import kr.ac.hongik.dsc2023.ydy.team1.core.architecture.dto.response.SearchItemResponseDTO;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
 @Getter
-public class KonbiniSearchItemResponseDTO extends SearchItemResponseDTO {
+public class KonbiniSearchItemResponseDTO<T extends SearchItem> extends SearchItemResponseDTO<T> {
 }
