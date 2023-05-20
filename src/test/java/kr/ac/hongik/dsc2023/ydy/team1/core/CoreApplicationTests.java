dbb95ae03c42e80019a7e515e7b35c2c9f877c09
robinjoon <robin980108@naver.com>
2023-05-20 21:31:03 +0900

    Test(KonbiniItemCreateService) : 상품 이미지 저장 테스트 코드 추가

--- src/test/java/kr/ac/hongik/dsc2023/ydy/team1/core/CoreApplicationTests.java
+++ src/test/java/kr/ac/hongik/dsc2023/ydy/team1/core/CoreApplicationTests.java
@@ -5,19 +5,27 @@ import kr.ac.hongik.dsc2023.ydy.team1.core.architecture.dto.request.SearchStreng
 import kr.ac.hongik.dsc2023.ydy.team1.core.architecture.service.ItemSearchService;
 import kr.ac.hongik.dsc2023.ydy.team1.core.konbini.dto.request.KonbiniItemCreateRequestDTO;
 import kr.ac.hongik.dsc2023.ydy.team1.core.konbini.dto.request.KonbiniSearchItemRequestDTO;
+import kr.ac.hongik.dsc2023.ydy.team1.core.konbini.dto.response.KonbiniItemCreateResponseDTO;
 import kr.ac.hongik.dsc2023.ydy.team1.core.konbini.dto.response.KonbiniSearchItem;
 import kr.ac.hongik.dsc2023.ydy.team1.core.konbini.model.KonbiniBrand;
 import kr.ac.hongik.dsc2023.ydy.team1.core.konbini.model.KonbiniPromotion;
 import kr.ac.hongik.dsc2023.ydy.team1.core.konbini.service.KonbiniItemCreateService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @SpringBootTest
 class CoreApplicationTests {
+	@Value("${item.img-test}")
+	String TEST_IMG;
 	@Autowired
 	private ItemSearchService<KonbiniSearchItem,KonbiniSearchItemRequestDTO> itemSearchService;
 	@Autowired
@@ -61,12 +69,10 @@ class CoreApplicationTests {
 	void tt(){
 		var list = itemSearchService.search(KonbiniSearchItemRequestDTO.builder()
 				.name("꼬깔콘").build());
-		for (KonbiniSearchItem searchItem : list.getSearchItems()) {
-			System.out.println(searchItem.getName());
-		}
+		list.getSearchItems().forEach(System.out::println);
 	}
 	@Test
-	void create(){
+	void create() throws IOException {
 		List<KonbiniItemCreateRequestDTO> list = new ArrayList<>();
 		KonbiniItemCreateRequestDTO konbiniItemCreateRequestDTO =
 				KonbiniItemCreateRequestDTO.builder()
@@ -74,6 +80,7 @@ class CoreApplicationTests {
 						.name("aaaa")
 						.promotion(KonbiniPromotion.TWO_PLUS_ONE)
 						.pricePerUnit(1000)
+						.picture(new MockMultiPartFile())
 						.build();
 		list.add(konbiniItemCreateRequestDTO);
 		list.add(KonbiniItemCreateRequestDTO.builder()
@@ -81,14 +88,54 @@ class CoreApplicationTests {
 				.brand(KonbiniBrand.GS25)
 				.promotion(KonbiniPromotion.ONE_PLUS_ONE)
 				.pricePerUnit(10000)
+				.picture(new MockMultiPartFile())
 				.build());
 
 		ItemsCreateRequestDTO<KonbiniItemCreateRequestDTO> dto = ItemsCreateRequestDTO.<KonbiniItemCreateRequestDTO>builder().itemList(list).build();
-		createService.create(dto);
-		var result = itemSearchService.search(KonbiniSearchItemRequestDTO.builder()
-				.name("꼬깔콘").build());
-		for (KonbiniSearchItem searchItem : result.getSearchItems()) {
-			System.out.println(searchItem.getName());
-		}
+		var result = (KonbiniItemCreateResponseDTO<KonbiniItemCreateRequestDTO>)createService.create(dto);
+		var failList = result.getFailList();
+		Assertions.assertEquals(failList, Collections.EMPTY_LIST);
+	}
+}
+class MockMultiPartFile implements MultipartFile {
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public String getOriginalFilename() {
+		return "test.jpg";
+	}
+
+	@Override
+	public String getContentType() {
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public long getSize() {
+		return 0;
+	}
+
+	@Override
+	public byte[] getBytes(){
+		return new byte[0];
+	}
+
+	@Override
+	public InputStream getInputStream(){
+		return null;
+	}
+
+	@Override
+	public void transferTo(File dest){
+		System.out.println("저장");
 	}
 }
