67cf63ec9e873f3429590fb3e3e4b91bc952410a
robinjoon <robin980108@naver.com>
2023-05-20 18:54:53 +0900

    Feat(KonbiniItemRepository) : 이름으로 검색 메서드 추가

--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/konbini/repository/KonbiniItemRepository.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/konbini/repository/KonbiniItemRepository.java
@@ -3,5 +3,8 @@ package kr.ac.hongik.dsc2023.ydy.team1.core.konbini.repository;
 import kr.ac.hongik.dsc2023.ydy.team1.core.konbini.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface KonbiniItemRepository extends JpaRepository<Item,Long> {
+    Optional<Item> findByName(String name);
 }
