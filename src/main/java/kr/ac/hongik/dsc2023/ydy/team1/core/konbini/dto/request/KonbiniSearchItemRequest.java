0940e68105a37314955f1693b150cedd3474fb76
robinjoon <robin980108@naver.com>
2023-11-21 00:24:59 +0900

    refactor : SearchItemRequest 삭제

--- src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/konbini/dto/request/KonbiniSearchItemRequest.java
+++ src/main/java/kr/ac/hongik/dsc2023/ydy/team1/core/konbini/dto/request/KonbiniSearchItemRequest.java
@@ -1,14 +1,19 @@
 package kr.ac.hongik.dsc2023.ydy.team1.core.konbini.dto.request;
 
-import kr.ac.hongik.dsc2023.ydy.team1.core.architecture.dto.request.SearchItemRequest;
+import kr.ac.hongik.dsc2023.ydy.team1.core.architecture.dto.request.SearchStrength;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 
 @Getter
 @ToString(callSuper = true)
-@SuperBuilder
+@Builder
 @NoArgsConstructor
-public class KonbiniSearchItemRequest extends SearchItemRequest {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class KonbiniSearchItemRequest {
+    private String name;
+    private SearchStrength strength;
 }
